a9c2995cd0e96340bf9c82720f684e4249328de9
cme2me <max.zhakov@gmail.com>
2021-11-16 16:15:39 +0300

    Second step done!

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,23 @@ public class Main {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
     public static void printThreeWords(){
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+    public static void checkSumSign(){
+        int a = 5;
+        int b = -40;
+        int c = a+b;
+
+        if(c >= 0){
+            System.out.println("Сумма положительная");
+        }
+        else{
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
