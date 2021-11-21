7e420d274d7fc93d1f5a1933dee4fb91d84d9eb4
cme2me <max.zhakov@gmail.com>
2021-11-21 19:48:03 +0300

    Made first task

--- src/hw2/HomeWorkTwo.java
+++ src/hw2/HomeWorkTwo.java
@@ -1,5 +1,18 @@
 package hw2;
 
 public class HomeWorkTwo {
+    public static void main(String[] args) {
+        from10to20(1,13);
+    }
+    public static void from10to20(int a, int b){
+        int sum = (a+b);
 
+        if(sum>=10 && sum<=20){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
 }
+
