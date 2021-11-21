b7489e8aef43966b5b33ed41b6be39310b00f3a0
cme2me <max.zhakov@gmail.com>
2021-11-21 19:58:30 +0300

    Made second task

--- src/hw2/HomeWorkTwo.java
+++ src/hw2/HomeWorkTwo.java
@@ -3,6 +3,7 @@ package hw2;
 public class HomeWorkTwo {
     public static void main(String[] args) {
         from10to20(1,13);
+        minusOrPluse(-10);
     }
     public static void from10to20(int a, int b){
         int sum = (a+b);
@@ -12,6 +13,13 @@ public class HomeWorkTwo {
         }
         else{
             System.out.println("false");
+        }
+    }
+    public static void minusOrPluse(int a) {
+        if (a > -1) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
         }
     }
 }
