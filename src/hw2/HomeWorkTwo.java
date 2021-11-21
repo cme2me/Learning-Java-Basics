ee4a68d10ada85416886e8141d28e02bd300c027
cme2me <max.zhakov@gmail.com>
2021-11-21 20:06:49 +0300

    3 task done

--- src/hw2/HomeWorkTwo.java
+++ src/hw2/HomeWorkTwo.java
@@ -4,6 +4,7 @@ public class HomeWorkTwo {
     public static void main(String[] args) {
         from10to20(1,13);
         minusOrPluse(-10);
+        fullOrNot(5);
     }
     public static void from10to20(int a, int b){
         int sum = (a+b);
@@ -21,6 +22,15 @@ public class HomeWorkTwo {
         } else {
             System.out.println("Число отрицательное");
         }
+    }
+    public static int fullOrNot(int a){
+        if(a<0){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+        return a;
     }
 }
 
