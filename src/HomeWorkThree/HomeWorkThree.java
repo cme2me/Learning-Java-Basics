2f9f585b064c08dc21fc7f113a283ed0de633d99
cme2me <max.zhakov@gmail.com>
2021-11-26 13:14:22 +0300

    Second step done

--- src/HomeWorkThree/HomeWorkThree.java
+++ src/HomeWorkThree/HomeWorkThree.java
@@ -5,6 +5,14 @@ public class HomeWorkThree {
     public static void main(String[] args) {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
         System.out.println(Arrays.toString(first(arr)));
+
+        int[] arr2 = new int[100];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr2));
+
+
     }
 
     public static int[] first(int[] array) {
@@ -13,4 +21,5 @@ public class HomeWorkThree {
         }
         return array;
     }
+
 }
