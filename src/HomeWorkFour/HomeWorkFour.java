3e4a8731d891b5b64711843b7c672c9121e310e6
cme2me <max.zhakov@gmail.com>
2021-11-28 16:04:54 +0300

    did method printMap

--- src/HomeWorkFour/HomeWorkFour.java
+++ src/HomeWorkFour/HomeWorkFour.java
@@ -19,15 +19,29 @@ public class HomeWorkFour {
     public static void main(String[] args) {
 
 
-        public static void initMap () {
-            map = new char[SIZE][SIZE];
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    map[i][j] = DOT_EMPTY;
-                }
+
+
+    }
+    public static void initMap () {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
             }
         }
-
-
+    }
+    public static void printMap() {
+        System.out.print("  ");
+        for (int i = 1; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i + 1 + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.printf("%c ", map[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
