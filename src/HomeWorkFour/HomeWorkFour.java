7ea9caa44c7ea37a06696d675d7607a5f471ce63
cme2me <max.zhakov@gmail.com>
2021-11-28 16:03:06 +0300

    did scanner and initMap

--- src/HomeWorkFour/HomeWorkFour.java
+++ src/HomeWorkFour/HomeWorkFour.java
@@ -1,7 +1,33 @@
 package HomeWorkFour;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HomeWorkFour {
+    static final int SIZE = 3;
+
+
+    static final char DOT_X = 'X';
+    static final char DOT_O = 'O';
+    static final char DOT_EMPTY = '.';
+
+    static char[][] map;
+
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
     public static void main(String[] args) {
+
+
+        public static void initMap () {
+            map = new char[SIZE][SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+
 
     }
 }
