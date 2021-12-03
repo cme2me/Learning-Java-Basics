569e24fc4cfca078845953bdae6b20218a827167
cme2me <max.zhakov@gmail.com>
2021-12-03 13:38:54 +0300

    Created MainApp and Employee, did FIO about person

---
+++ src/HomeWorkFive/Employee.java
@@ -0,0 +1,11 @@
+package HomeWorkFive;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+
+    private int salary;
+    private int phone;
+    private int age;
+}
---
+++ src/HomeWorkFive/MainApp.java
@@ -0,0 +1,7 @@
+package HomeWorkFive;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
