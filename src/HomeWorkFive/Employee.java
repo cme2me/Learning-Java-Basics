a16ef89293fe6692cfaf17425e49d662cacd4c8e
cme2me <max.zhakov@gmail.com>
2021-12-03 13:51:48 +0300

    Loop fori done

--- src/HomeWorkFive/Employee.java
+++ src/HomeWorkFive/Employee.java
@@ -7,5 +7,14 @@ public class Employee {
 
     private int salary;
     private int phone;
-    private int age ;
+    private int age;
+
+    public Employee(String FIO, String position, String email, int phone, int salary, int age){
+        this.name = FIO;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 }
