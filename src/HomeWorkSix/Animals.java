892c69782cb3e398fd2b39d309290db4e7a12983
cme2me <max.zhakov@gmail.com>
2021-12-08 15:03:14 +0300

    void Swim

--- src/HomeWorkSix/Animals.java
+++ src/HomeWorkSix/Animals.java
@@ -30,4 +30,19 @@ public class Animals {
             System.out.printf("%s %s не смог пробежать %d метров\n", type, name, distantion);
         }
     }
+
+    public void swim(int distantion){
+        if(maxSwim == 0 ){
+            System.out.printf("%s %s не умеет бегать\n", type, name);
+            return;
+        }
+        else if(distantion < 0 ){
+            System.out.println("Для плавания в обратном направлении введите то же число");
+            return;
+        }
+        else{
+            System.out.printf("%s %s не смог проплыть %d метров\n", type, name, distantion);
+        }
+
+    }
 }
