57e4be90607e6b0e0a5b5e5bcd18fffa1d8b79f5
cme2me <max.zhakov@gmail.com>
2021-12-08 14:25:55 +0300

    Constructure

--- src/HomeWorkSix/Animals.java
+++ src/HomeWorkSix/Animals.java
@@ -1,4 +1,16 @@
 package HomeWorkSix;
 
 public class Animals {
+    String name;
+    String type;
+
+    int maxRun;
+    int maxSwim;
+
+    public Animals(String name, String type, int maxRun, int maxSwim){
+        this.name = name;
+        this.type = type;
+        this.maxRun = maxRun;
+        this.maxSwim = maxSwim;
+    }
 }
