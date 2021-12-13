62fe5f7f4e6d9b2186fa1a98f146e922a6e3be36
cme2me <max.zhakov@gmail.com>
2021-12-13 12:36:07 +0300

    Created Box, Dog, Cat, Main and Plate

--- src/HomeWorkSeven/Dog.java
+++ src/HomeWorkSeven/Dog.java
@@ -1,18 +1,4 @@
 package HomeWorkSeven;
 
 public class Dog {
-    private String name;
-
-    public Dog(String name) {
-        this.name = name;
-    }
-
-    public void scare(Cat cat) {
-        System.out.println("Dog " + name + " scare cat "+ cat.getName() );
-        cat.fear(this);
-    }
-
-    public String getName() {
-        return name;
-    }
 }
--- src/HomeWorkSeven/Plate.java
+++ src/HomeWorkSeven/Plate.java
@@ -1,31 +1,4 @@
 package HomeWorkSeven;
 
 public class Plate {
-    private int food;
-
-    public Plate(int food) {
-        this.food = food;
-    }
-
-    public boolean decreaseFood(int amount) {
-        if (food >= amount) {
-            food -= amount;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public int getFood() {
-        return food;
-    }
-
-    public boolean addFood(int amount) {
-        if (amount < 0) {
-            return false;
-        } else {
-            food += amount;
-            return true;
-        }
-    }
 }
